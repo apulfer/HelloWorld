c034250e69da60624ee571c63da3ca9ddd9a08af
apulfer <apulfer@unomaha.edu>
2014-09-15 20:33:06 -0500

    Update settings.py

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -1,3 +1,14 @@
+"""
+Django settings for gettingstarted project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import dj_database_url
 
